11e8f791e105cc541b0afd388c6fb0788f72c708
Thiago Corrêa <thiago.4747@gmail.com>
2020-10-01 13:11:03 -0300

    Bug fixes and functions deployment

--- Compiler/lexical.py
+++ Compiler/lexical.py
@@ -1,28 +1,55 @@
-def lexical(f,i):
+def expr(item):
+    aux = str(dictionary[item])
+    escape,exp = aux.split('[\'')
+    exp,escape = exp.split('\']')
+    return str(exp)
 
+def lexical(ind):
     aux = ''
-    line = f.readline()
-    if line[0].isalpha():
-        while line[i].isalpha():
-            aux = aux + line
-            i += 1
-        return (aux + ' ' + str(i))
+    exp = expr(ind+1)
+    nex = ind
+    lenght = len(exp)
+    print (exp[ind])
+    while True:
+        if exp[ind].isalpha():
+                while exp[nex].isalpha():
+                    if exp[nex+1].isalpha() or exp[nex+1].isdigit() or exp[nex+1] == '_':
+                        nex += 1
+                    else:
+                        break
+                return (str(nex) + str(ind))
+        else:
+            print ("invalid id token, in line " + str(ind))
+            return '$'
+    return '$'
 
 def spliter(token):
+    if token == '':
+        return 1
     token,stop = token.split()
     return int(stop)
 
 def tamanho(f):
+    global lines
     lines = 0
-    for line in f:
-        print(line, end='')
-        lines += 1
-    return lines
+    mat = {}
 
-#inicio
-f = open("Compiler\entry.txt")
-try:
-    token = lexical(f,0)
-    spliter(token)
-finally:
-    f.close()
+    for line in f:
+        lines += 1
+        mat[lines] = line.splitlines()
+    return (mat)
+
+#inicio        
+with open('Compiler\entry.txt') as f:
+    try:
+        global dictionary
+        dictionary = tamanho(f)
+        while True:
+            token = ''
+            token = lexical(0)
+            print (token)
+            token = ''
+            if token == '$' or token == '':
+                break
+    finally:
+        f.close()
