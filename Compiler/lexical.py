716be0b757dece5636f2e1388e1569f0527637c3
Thiago Corrêa <thiago.4747@gmail.com>
2020-10-01 22:22:30 -0300

    Rebuild the code, create new methods, refactored the lines

--- Compiler/lexical.py
+++ Compiler/lexical.py
@@ -1,55 +1,112 @@
-def expr(item):
-    aux = str(dictionary[item])
-    escape,exp = aux.split('[\'')
-    exp,escape = exp.split('\']')
-    return str(exp)
+def num_token(act,nex):
+    pass
 
-def lexical(ind):
-    aux = ''
-    exp = expr(ind+1)
-    nex = ind
-    lenght = len(exp)
-    print (exp[ind])
-    while True:
-        if exp[ind].isalpha():
-                while exp[nex].isalpha():
-                    if exp[nex+1].isalpha() or exp[nex+1].isdigit() or exp[nex+1] == '_':
-                        nex += 1
-                    else:
+def lit_token(act,nex):
+    if (act == '\"'):
+        return True
+    else:
+        return False        
+    
+def id_token(act,nex):
+    pass
+
+def comment_token(act,nex):
+    pass
+
+def eof_token(act,nex):
+    pass
+
+def opr_token(act,nex):
+    pass
+
+def rcb_token(act,nex):
+    pass
+
+def opm_token(act,nex):
+    pass
+
+def ab_p_token(act,nex):
+    pass
+
+def fc_p_token(act,nex):
+    pass
+
+def pt_v_token(act,nex):
+    pass
+
+def error(err):
+    print (err)
+
+def lexical(line_num,line):
+    ind = 0
+    itr = 0
+    aux = 0
+
+    tam = len(line)
+    while itr < tam - 1:
+        actual = line[itr]
+        
+        if (line[itr+1] != '\n'):
+            nex = line[itr+1]
+        itr += 1
+
+        if (num_token(actual,nex)):
+            dictionary['num'] = num_token(actual,nex)
+
+        elif (lit_token(actual,nex)):
+            if (actual == '\"' and nex != '\"'):
+                aux = itr
+                while aux < tam - 1:
+                    if (nex == '\"'):
                         break
-                return (str(nex) + str(ind))
+                    nex = line[aux+1]
+                if (aux == tam - 1):
+                    error("Literal constant is not closed, in line: " + str(line_num))
+                    return
+            dictionary['lit'] = lit_token(actual,nex)
+
+        elif (id_token(actual,nex)):
+            dictionary['id'] = id_token(actual,nex)
+        
+        elif (comment_token(actual,nex)):
+            #recognize but do nothing
+            pass
+            
+        elif (eof_token(actual,nex)):
+            dictionary['eof'] = eof_token(actual,nex)
+            
+        elif (opr_token(actual,nex)):
+            dictionary['opr'] = opr_token(actual,nex)
+            
+        elif (rcb_token(actual,nex)):
+            dictionary['rcb'] = rcb_token(actual,nex)
+                        
+        elif (opm_token(actual,nex)):
+            dictionary['opm'] = opm_token(actual,nex)
+                        
+        elif (ab_p_token(actual,nex)):
+            dictionary['abp'] = ab_p_token(actual,nex)
+                        
+        elif (fc_p_token(actual,nex)):
+            dictionary['fcp'] = fc_p_token(actual,nex)
+                        
+        elif (pt_v_token(actual,nex)):
+            dictionary['ptv'] = pt_v_token(actual,nex)
+                        
         else:
-            print ("invalid id token, in line " + str(ind))
-            return '$'
-    return '$'
-
-def spliter(token):
-    if token == '':
-        return 1
-    token,stop = token.split()
-    return int(stop)
-
-def tamanho(f):
-    global lines
-    lines = 0
-    mat = {}
-
-    for line in f:
-        lines += 1
-        mat[lines] = line.splitlines()
-    return (mat)
-
-#inicio        
+            error(actual)
+            #implement it later
+            pass
+            
+#inicio
 with open('Compiler\entry.txt') as f:
+    global dictionary
     try:
-        global dictionary
-        dictionary = tamanho(f)
-        while True:
-            token = ''
-            token = lexical(0)
-            print (token)
-            token = ''
-            if token == '$' or token == '':
-                break
+        line_num = 1
+        line = f.readline()
+        while line:
+            lexical(line_num,line)
+            line  = f.readline()
+            line_num += 1
     finally:
         f.close()
